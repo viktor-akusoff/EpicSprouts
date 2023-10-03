b31e4f10ab090375e4fdd107c964218fe31af70d
viktor-akusoff <kirill.voloshin.98@ya.ru>
2023-10-04 01:58:09 +0300

    adding polyline

--- client/entities.py
+++ client/entities.py
@@ -1,5 +1,8 @@
 import itertools
+import numpy as np
+import pygame as pg
 from dataclasses import dataclass, field
+from typing import Tuple, List
 
 
 @dataclass
@@ -13,3 +16,56 @@ class Node:
     def __post_init__(self):
         self.id = next(Node.id_itter)
         Node.instances.append(self)
+
+    def draw(self, screen):
+        pg.draw.circle(screen, (0, 0, 0), (self.x, self.y), 5)
+
+
+@dataclass
+class PolyLine:
+    instances = []
+    id_itter = itertools.count()
+    vertexes: List[Tuple[int, int]] = field(init=False)
+    split_distance: float = field(default=5)
+    id: int = field(init=False)
+
+    def __post_init__(self):
+        self.vertexes = []
+        self.id = next(PolyLine.id_itter)
+        PolyLine.instances.append(self)
+
+    def push_vertex(self, x: int, y: int):
+        self.vertexes.append((x, y))
+
+    def is_edge_end(self, x, y) -> bool:
+        last_x, last_y = self.vertexes[-1]
+        distance = np.sqrt(
+            np.power(x - last_x, 2) +
+            np.power(y - last_y, 2)
+        )
+
+        return distance >= self.split_distance
+
+    def draw(self, screen):
+        if len(self.vertexes) < 2:
+            return
+
+        previous_vertex = self.vertexes[0]
+
+        colors = [
+            (70, 125, 70),
+            (125, 70, 70)
+        ]
+
+        counter = 0
+
+        for vertex in self.vertexes[1:]:
+            pg.draw.line(
+                screen,
+                colors[counter % 2],
+                previous_vertex,
+                vertex,
+                3
+            )
+            counter += 1
+            previous_vertex = vertex
--- start_client.py
+++ start_client.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from client.entities import Node
+from client.entities import PolyLine
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,19 +10,26 @@ if __name__ == "__main__":
     screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
     running = True
+    line = None
     while running:
-
+        pos = pg.mouse.get_pos()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             elif event.type == pg.MOUSEBUTTONDOWN:
-                pos = pg.mouse.get_pos()
-                Node(*pos)
+                line = PolyLine(25)
+                line.push_vertex(*pos)
+            elif (event.type == pg.MOUSEBUTTONUP) and line:
+                line.push_vertex(*pos)
+                line = None
+
+        if line and line.is_edge_end(*pos):
+            line.push_vertex(*pos)
 
         screen.fill((255, 255, 255))
 
-        for node in Node.instances:
-            pg.draw.circle(screen, (0, 0, 0), (node.x, node.y), 5)
+        for polyline in PolyLine.instances:
+            polyline.draw(screen)
 
         pg.display.flip()
 
