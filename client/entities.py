eb33c3a567555795bd49a2e50d69602931375d39
viktor-akusoff <kirill.voloshin.98@ya.ru>
2023-10-10 16:35:02 +0300

    making single rectangle

--- client/entities.py
+++ client/entities.py
@@ -2,7 +2,7 @@ import itertools
 import numpy as np
 import pygame as pg
 from dataclasses import dataclass, field
-from typing import Tuple, List, Dict
+from typing import Tuple, List
 
 
 @dataclass
@@ -31,30 +31,17 @@ class RectCheck:
     x2: int = field(init=False, default=0)
     y2: int = field(init=False, default=0)
 
-    start_x: int = field(init=False, default=0)
-    start_y: int = field(init=False, default=0)
-
-    end_x: int = field(init=False, default=0)
-    end_y: int = field(init=False, default=0)
-
-    counter: int = field(init=False, default=0)
-
     def draw(self, screen):
         rect = [self.x1, self.y1, self.x2 - self.x1, self.y2 - self.y1]
         pg.draw.rect(screen, (0, 0, 0), rect, 1)
 
     def push_vertex(self, x, y):
-        self.counter += 1
         if self.empty:
             self.empty = False
             self.x1 = x
             self.y1 = y
             self.x2 = x
             self.y2 = y
-            self.start_x = x
-            self.start_y = y
-            self.end_x = x
-            self.end_y = y
             return
 
         if self.x1 > x:
@@ -79,66 +66,22 @@ class RectCheck:
 
 
 @dataclass
-class RectSpace:
-    start_x: int = field(init=False, default=0)
-    start_y: int = field(init=False, default=0)
-    vertex_counter: int = field(init=False, default=0)
-    power_counter: int = field(init=False, default=1)
-    rectangles: Dict[int, List[RectCheck]] = field(init=False)
-
-    def __post_init__(self):
-        self.rectangles = {1: [RectCheck()]}
-
-    def push_vertex(self, x, y):
-
-        for key in self.rectangles:
-            self.rectangles[key][-1].push_vertex(x, y)
-
-        if self.vertex_counter == self.power_counter:
-            rect = self.rectangles[self.power_counter][-1]
-            self.power_counter *= 2
-            self.rectangles[self.power_counter] = [
-                RectCheck()
-            ]
-            self.rectangles[self.power_counter][0].push_vertex(
-                 rect.x1,
-                 rect.y1
-            )
-            self.rectangles[self.power_counter][0].push_vertex(
-                rect.x2,
-                rect.y2
-            )
-
-        for key in self.rectangles:
-            if self.vertex_counter and not (self.vertex_counter % key):
-                self.rectangles[key].append(RectCheck())
-                self.rectangles[key][-1].push_vertex(x, y)
-
-        self.vertex_counter += 1
-
-    def draw(self, screen):
-        for _, value in self.rectangles.items():
-            for rect in value:
-                rect.draw(screen)
-
-
-@dataclass
 class PolyLine:
     instances = []
     id_itter = itertools.count()
     vertexes: List[Tuple[int, int]] = field(init=False)
-    rect_space: RectSpace = field(init=False)
+    rect_check: RectCheck = field(init=False)
     split_distance: float = field(default=5)
     id: int = field(init=False)
 
     def __post_init__(self):
         self.vertexes = []
-        self.rect_space = RectSpace()
+        self.rect_check = RectCheck()
         self.id = next(PolyLine.id_itter)
         PolyLine.instances.append(self)
 
     def push_vertex(self, x: int, y: int):
-        self.rect_space.push_vertex(x, y)
+        self.rect_check.push_vertex(x, y)
         self.vertexes.append((x, y))
 
     def is_edge_end(self, x, y) -> bool:
@@ -181,6 +124,6 @@ class PolyLine:
                 3
             )
             if debug:
-                self.rect_space.draw(screen)
+                self.rect_check.draw(screen)
             counter += 1
             previous_vertex = vertex
